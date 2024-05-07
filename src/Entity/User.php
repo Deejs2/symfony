ecab86487f9588d4f52778b4b028e7f4ae2e6f3c
Dhiraj <jireldhiraj123@gmail.com>
2024-05-07 19:47:08 +0545

    implement serialization for not showing password and email for users in GET api

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,33 +9,39 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-#[ApiResource]
+#[ApiResource(normalizationContext: ['groups' => ['get']])]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['get'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255, unique: true)]
+    #[Groups(['get'])]
     private ?string $username = null;
 
     #[ORM\Column(length: 255)]
     private ?string $password = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['get'])]
     private ?string $name = null;
 
     #[ORM\Column(length: 255, unique: true)]
     private ?string $email = null;
 
     #[ORM\OneToMany(targetEntity: BlogPost::class, mappedBy: 'author')]
+    #[Groups(['get'])]
     private Collection $posts;
 
     #[ORM\OneToMany(targetEntity: Comment::class, mappedBy: 'author')]
+    #[Groups(['get'])]
     private Collection $comments;
 
     public function __construct()
